107f7fca4ee9d180a1ed5ece93fdd91bcaeaa325
Pim Duin <pimduin@gmail.com>
2013-05-28 16:22:20 +0200

    Added copyright notice

---
+++ cpp/core/src/zxing/oned/UPCEANExtension2Reader.h
@@ -0,0 +1,46 @@
+// -*- mode:c++; tab-width:2; indent-tabs-mode:nil; c-basic-offset:2 -*-
+#ifndef __ZXingWidget__UPCEANExtension2Reader__
+#define __ZXingWidget__UPCEANExtension2Reader__
+
+/*
+ *  Copyright 2013 ZXing authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <string>
+#include <zxing/common/Counted.h>
+#include <zxing/common/BitArray.h>
+#include <zxing/Result.h>
+
+namespace zxing {
+	namespace oned {
+		class UPCEANExtension2Reader {
+		private:
+			std::string decodeRowStringBuffer;
+			std::vector<int> decodeMiddleCounters;
+			
+		public:
+			UPCEANExtension2Reader();
+			
+			Ref<Result> decodeRow(int rowNumber, Ref<BitArray> row, int rangeStart, int rangeEnd);
+			int decodeMiddle(Ref<BitArray> row,
+											 int rangeStart,
+											 int rangeEnd,
+											 std::string& resultString);
+		};
+	}
+}
+
+
+#endif /* defined(__ZXingWidget__UPCEANExtension2Reader__) */
